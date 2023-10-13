f0f8d28b8afb9bedfb5536ad93d8a40cf928c29b
hanifarhanz <52771731+hanfarhanzoo@users.noreply.github.com>
2023-10-13 20:24:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function hanifarhanMouseEnter(){
+    document.getElementById("hanifarhan").innerHTML = "___7697___";
+    document.getElementById("hanifarhan").style.color = "black";
+    document.getElementById("hanifarhan").style.backgroundColor = "white";
+
+}
+function hanifarhanMouseLeave(){
+    document.getElementById("hanifarhan").innerHTML = "Hanifarhan";
+    document.getElementById("hanifarhan").style.color = "white";
+    document.getElementById("hanifarhan").style.background = "none";
+}
